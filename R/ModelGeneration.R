bc85ff7994f3a14e0289fc45bdfe1e0216b28c69
orionw <wellerorion@gmail.com>
2018-12-27 12:36:21 -0800

    removed extra output

--- R/ModelGeneration.R
+++ R/ModelGeneration.R
@@ -1,7 +1,7 @@
 #' ModelComparisons()
 ModelComparison <- function(ModelList, multi_class, force_prepped, diff_names="none") {
   # we can add our own integrity checks
-  comparison <- "Model Comparison Object"
+  comparison <- list()
   if (diff_names == "none") {
     model_list <- list(svmLinear = ModelList[["svmLinear"]],
                   neuralNet = ModelList[["neuralNet"]],
@@ -12,15 +12,12 @@ ModelComparison <- function(ModelList, multi_class, force_prepped, diff_names="n
     model_list = ModelList
     names(model_list) <- diff_names
   }
-  print("HERE in end")
-  # class can be set using class() or attr() function
-  comparison$model_list <- model_list
+  # give object its members
+  comparison$model_list <- list()
+  comparison$model_list <- as.list(model_list)
   comparison$force_prepared = force_prepped
   comparison$.multi_class <- multi_class
-  print(multi_class)
-  print(comparison$.multi_class)
   class(comparison) <- "ModelComparison"
-  print(class(comparison))
   return(comparison)
 }
 
@@ -40,14 +37,12 @@ plot.ModelComparison <- function(object, training_data, labels, predictions="emp
     training_data = prepData(training_data)
   }
 
-  print("In plotting function")
   if (object$.multi_class == TRUE) {
     # do stuff later
-    print("IS MULTICLASS")
+    message("dataset is multi-class")
   } else {
       if (predictions == "empty") {
         # predictions somehow failed to happen - predict in here
-        print("predictions are emtpy")
         pred_basic <- predict(object$model_list, newdata=training_data, type="prob")
       } else {
         # use the given predictions
@@ -83,21 +78,17 @@ plot.ModelComparison <- function(object, training_data, labels, predictions="emp
 #' @examples
 #' predict()
 predict.ModelComparison <- function(object, training_data) {
-  print("I am predicting function")
   predictions_list = list()
   i = 0
   if (object$.multi_class) {
     # do something TODO
-    print("IT is MULTICLASS")
+    message("predicting on a multi-class outcome")
     for (model in object$model_list) {
       i = i + 1
     }
   } else {
     # predict over the list of models
     pred_basic <- predict(object$model_list, newdata=training_data, type="prob")
-    print("Dimensions are")
-    print(dim(pred_basic))
-    print(length(pred_basic[0]))
     return(pred_basic)
   }
 }
@@ -123,7 +114,6 @@ prepData <- function(training_set) {
 
 buildModels <- function(training_data, training_classes, trctrl,
                      tune_length, multi_class, force_prepared = F ) {
-  print("I am in buildModels")
   out <- tryCatch(
     {
       svmLinear <- caret::train(training_data, training_classes, method = "svmLinear",
@@ -131,15 +121,15 @@ buildModels <- function(training_data, training_classes, trctrl,
                                 preProcess = c("center", "scale"),
                                 tuneLength = tune_length)
 
-      neuralNet <- caret::train(training_data, training_classes, method = "nnet",
+      # capture output to reduce the excessive output this package gives in training
+      capture.output(neuralNet <- caret::train(training_data, training_classes, method = "nnet",
                                 trControl=trctrl,
                                 preProcess = c("center", "scale"),
-                                tuneLength = tune_length)
+                                tuneLength = tune_length))
 
       # prepare data for a GLMNET
       train <- data.frame(training_data, training_classes)
       x.m <- model.matrix( ~.+0, training_data)
-      #print(head(x.m))
       glmnet <- caret::train(x.m, training_classes,  method = "glmnet",
                              trControl=trctrl,
                              preProcess = c("center", "scale"),
@@ -161,7 +151,7 @@ buildModels <- function(training_data, training_classes, trctrl,
         glm_model = NULL
       }
 
-      print("Done Processing")
+      message("Models are done building")
 
       # get the visuals for all the models
       modelVec = list(svmLinear, neuralNet, glmnet, randomForest, glm_model)
@@ -192,8 +182,6 @@ buildModels <- function(training_data, training_classes, trctrl,
 #' @examples
 #' getModelComparisons()
 getModelComparisons <-function(trainingSet, training_classes_input, validation="80/20", modelList=NULL, dataSetSize="small") {
-  print("In Function")
-
   # check to see if function is good
   is_prepped <- sapply(trainingSet, function(x) (is.numeric(x) || length(levels(x)) <= 2))
 
@@ -234,7 +222,7 @@ getModelComparisons <-function(trainingSet, training_classes_input, validation="
     }
   }
 
-  print("validation method complete")
+  message("Settings configured successfully")
   tune_length = 1
 
   modelVec = buildModels(training_data, training_classes, trctrl,
--- tests/testthat/test_errors.R
+++ tests/testthat/test_errors.R
@@ -1,11 +1,8 @@
 context("Conversion and Error Checking")
 
 # load the libraries
-library(mlbench)
-library(plyr)
-library(ggplot2)
 library(BestModel)
-library(caret)
+
 
 test_that("Convert a list of models and plot it", {
   expect_equal(0, 0)
